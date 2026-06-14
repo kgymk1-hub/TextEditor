ec047407e69aa30e42177e310987d24789ed1455
kgymk1-hub <kgym.k1@gmail.com>
2026-06-14 22:00:01 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,64 @@
+const CACHE_NAME = "pocket-text-editor-v25";
+
+const FILES_TO_CACHE = [
+  "./",
+  "index.html",
+  "css/style.css",
+  "js/env.js",
+  "js/storage-service.js",
+  "js/tabs.js",
+  "js/editor.js",
+  "js/file-service.js",
+  "js/app.js",
+  "manifest.json",
+  "icons/icon.svg",
+  "libs/jszip.min.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+
+          return null;
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request).then((networkResponse) => {
+        return networkResponse;
+      });
+    })
+  );
+});
+
+/* SERVICE_WORKER_MARKER_EDITOR_SPLIT_V25_2026_06_07 */
